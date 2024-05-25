4c14cc20672f3cfaf6407b5303ba9bba81b09c8c
Hoobinoo <julian@manders-jones.net>
2024-05-26 00:28:59 +0100

    Security Fix

--- public/js/firebaseInit.js
+++ public/js/firebaseInit.js
@@ -1,13 +1,13 @@
 // firebaseInit.js
 const firebaseConfig = {
-    apiKey: "AIzaSyD_potT3TgBJvHKhhPSKNQ-kR84BPfdBu8",
-    authDomain: "braintrain-3bf0f.firebaseapp.com",
-    databaseURL: "https://braintrain-3bf0f-default-rtdb.firebaseio.com",
-    projectId: "braintrain-3bf0f",
-    storageBucket: "braintrain-3bf0f.appspot.com",
-    messagingSenderId: "786766490017",
-    appId: "1:786766490017:web:2e11eb3f0b450ad9b503e0",
-    measurementId: "G-BSCSQCBFQW"
+    apiKey: REACT_APP_API_KEY,
+    authDomain: REACT_APP_AUTH_DOMAIN,
+    databaseURL: REACT_APP_DATABASE_URL,
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket: REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+    appId: REACT_APP_APP_ID,
+    measurementId: REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
