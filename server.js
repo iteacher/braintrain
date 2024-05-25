4a5b4995cbe13da29534c769ac07f7d4f07c4b0b
Hoobinoo <julian@manders-jones.net>
2024-05-26 00:23:30 +0100

    Ignore .env - security patch for db connections

--- server.js
+++ server.js
@@ -4,33 +4,20 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React app's build directory
-app.use('/braintrain/build', express.static(path.join(__dirname, 'build')));
-app.use('/braintrain/static', express.static(path.join(__dirname, 'build/static')));
-app.use('/braintrain/js', express.static(path.join(__dirname, 'build/static/js')));
-app.use('/braintrain/css', express.static(path.join(__dirname, 'build/static/css')));
-app.use('/braintrain/styles', express.static(path.join(__dirname, 'build/static/styles')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// Serve static files from the public directory
-app.use('/braintrain', express.static(path.join(__dirname, 'public')));
-
-// Catch-all route
-app.get('/braintrain/*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-//Error handling
+// Error handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
 
-//Activate the node
-app.listen(() => {
-    console.log('Server is running.');
+// Activate the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
-
