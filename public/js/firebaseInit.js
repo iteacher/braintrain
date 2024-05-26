4f1c53e9ee303b8dcc6e2f502650541608e0942d
Hoobinoo <julian@manders-jones.net>
2024-05-26 23:08:12 +0100

    FirebaseInit Fix

--- public/js/firebaseInit.js
+++ public/js/firebaseInit.js
@@ -7,15 +7,11 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_REACT_APP_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_REACT_APP_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.database().ref().child('.info/connected').on('value', (snapshot) => {
-  const connected = snapshot.val();
-  console.log(`Connected: ${connected}`);
-});
 
 console.log("Firebase initialized:", firebase);
 
@@ -25,3 +21,12 @@ window.db = firebase.database();
 window.GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
 
 console.log("Firebase Auth initialized:", auth);
+
+const API_KEY = process.env.NEXT_PUBLIC_REACT_APP_API_KEY;
+const AUTH_DOMAIN = process.env.NEXT_PUBLIC_REACT_APP_AUTH_DOMAIN;
+const DATABASE_URL = process.env.NEXT_PUBLIC_REACT_APP_DATABASE_URL;
+const PROJECT_ID = process.env.NEXT_PUBLIC_REACT_APP_PROJECT_ID;
+const STORAGE_BUCKET = process.env.NEXT_PUBLIC_REACT_APP_STORAGE_BUCKET;
+const MESSAGING_SENDER_ID = process.env.NEXT_PUBLIC_REACT_APP_MESSAGING_SENDER_ID;
+const APP_ID = process.env.NEXT_PUBLIC_REACT_APP_APP_ID;
+const MEASUREMENT_ID = process.env.NEXT_PUBLIC_REACT_APP_MEASUREMENT_ID;
