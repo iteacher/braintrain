4d6aaca894cc183ff20bbffbdc11bba5bb54b54d
Hoobinoo <julian@manders-jones.net>
2024-05-26 10:22:34 +0100

    KEYS BACK

--- public/js/firebaseInit.js
+++ public/js/firebaseInit.js
@@ -1,13 +1,13 @@
 // firebaseInit.js
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyD_potT3TgBJvHKhhPSKNQ-kR84BPfdBu8",
+    authDomain: "braintrain-3bf0f.firebaseapp.com",
+    databaseURL: "https://braintrain-3bf0f-default-rtdb.firebaseio.com",
+    projectId: "braintrain-3bf0f",
+    storageBucket: "braintrain-3bf0f.appspot.com",
+    messagingSenderId: "786766490017",
+    appId: "1:786766490017:web:2e11eb3f0b450ad9b503e0",
+    measurementId: "G-BSCSQCBFQW"
 };
 
 // Initialize Firebase
