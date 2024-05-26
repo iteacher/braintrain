9e42d41367db8ec5064850be37011c2bf0ddcb8b
Hoobs <julian@manders-jones.net>
2024-05-26 17:08:00 +0200

    Web host in sync

---
+++ public/js/KEYS.js
@@ -0,0 +1,12 @@
+// apiKeys.js
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyD_potT3TgBJvHKhhPSKNQ-kR84BPfdBu8",
+  authDomain: "braintrain-3bf0f.firebaseapp.com",
+  databaseURL: "https://braintrain-3bf0f-default-rtdb.firebaseio.com",
+  projectId: "braintrain-3bf0f",
+  storageBucket: "braintrain-3bf0f.appspot.com",
+  messagingSenderId: "786766490017",
+  appId: "1:786766490017:web:2e11eb3f0b450ad9b503e0",
+  measurementId: "G-BSCSQCBFQW"
+};
