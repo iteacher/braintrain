e02a7a790313ffd9119d0fd33b757bed08d56dac
Hoobinoo <julian@manders-jones.net>
2024-05-26 15:20:25 +0100

    Fix

--- public/js/firebaseInit.js
+++ public/js/firebaseInit.js
@@ -1,27 +1,24 @@
-// firebaseInit.js
-const firebaseConfig = {
-    apiKey: "AIzaSyD_potT3TgBJvHKhhPSKNQ-kR84BPfdBu8",
-    authDomain: "braintrain-3bf0f.firebaseapp.com",
-    databaseURL: "https://braintrain-3bf0f-default-rtdb.firebaseio.com",
-    projectId: "braintrain-3bf0f",
-    storageBucket: "braintrain-3bf0f.appspot.com",
-    messagingSenderId: "786766490017",
-    appId: "1:786766490017:web:2e11eb3f0b450ad9b503e0",
-    measurementId: "G-BSCSQCBFQW"
-};
+(async () => {
+    try {
+        const response = await fetch('/firebaseConfig.json');
+        const firebaseConfig = await response.json();
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.database().ref().child('.info/connected').on('value', (snapshot) => {
-    const connected = snapshot.val();
-    console.log(`Connected: ${connected}`);
-});
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        firebase.database().ref().child('.info/connected').on('value', (snapshot) => {
+            const connected = snapshot.val();
+            console.log(`Connected: ${connected}`);
+        });
 
-console.log("Firebase initialized:", firebase);
+        console.log("Firebase initialized:", firebase);
 
-// Make Firebase services globally accessible
-window.auth = firebase.auth();
-window.db = firebase.database();
-window.GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
+        // Make Firebase services globally accessible
+        window.auth = firebase.auth();
+        window.db = firebase.database();
+        window.GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
 
-console.log("Firebase Auth initialized:", auth);
+        console.log("Firebase Auth initialized:", window.auth);
+    } catch (error) {
+        console.error('Error fetching Firebase config:', error);
+    }
+})();
