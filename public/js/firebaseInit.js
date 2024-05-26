0d393b769ebd8aa25a818cbefc2b888edb1c3f85
Hoobinoo <julian@manders-jones.net>
2024-05-26 18:19:09 +0100

    Firebase ENV

--- public/js/firebaseInit.js
+++ public/js/firebaseInit.js
@@ -1,27 +1,28 @@
-// firebaseInit.js
-const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_REACT_APP_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_REACT_APP_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_REACT_APP_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_REACT_APP_MEASUREMENT_ID
-};
+// Fetch the Firebase configuration from firebaseConfig.json
+fetch('/firebaseConfig.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(config => {
+    // Initialize Firebase
+    firebase.initializeApp(config);
+    firebase.database().ref().child('.info/connected').on('value', (snapshot) => {
+      const connected = snapshot.val();
+      console.log(`Connected: ${connected}`);
+    });
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.database().ref().child('.info/connected').on('value', (snapshot) => {
-    const connected = snapshot.val();
-    console.log(`Connected: ${connected}`);
-});
+    console.log("Firebase initialized:", firebase);
 
-console.log("Firebase initialized:", firebase);
+    // Make Firebase services globally accessible
+    window.auth = firebase.auth();
+    window.db = firebase.database();
+    window.GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
 
-// Make Firebase services globally accessible
-window.auth = firebase.auth();
-window.db = firebase.database();
-window.GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
-
-console.log("Firebase Auth initialized:", auth);
+    console.log("Firebase Auth initialized:", auth);
+  })
+  .catch(error => {
+    console.error('Error fetching Firebase configuration:', error);
+  });
