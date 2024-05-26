f8859c47b42ead734e533a10ed096bf1494b0461
Hoobinoo <julian@manders-jones.net>
2024-05-26 23:16:52 +0100

    firebase

--- public/js/firebaseInit.js
+++ public/js/firebaseInit.js
@@ -1,13 +1,13 @@
 // firebaseInit.js
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_REACT_APP_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_REACT_APP_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_REACT_APP_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyD_potT3TgBJvHKhhPSKNQ-kR84BPfdBu8",
+  authDomain: "braintrain-3bf0f.firebaseapp.com",
+  databaseURL: "https://braintrain-3bf0f-default-rtdb.firebaseio.com",
+  projectId: "braintrain-3bf0f",
+  storageBucket: "braintrain-3bf0f.appspot.com",
+  messagingSenderId: "786766490017",
+  appId: "1:786766490017:web:2e11eb3f0b450ad9b503e0",
+  measurementId: "G-BSCSQCBFQW"
 };
 
 // Initialize Firebase
@@ -21,12 +21,3 @@ window.db = firebase.database();
 window.GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
 
 console.log("Firebase Auth initialized:", auth);
-
-const API_KEY = process.env.NEXT_PUBLIC_REACT_APP_API_KEY;
-const AUTH_DOMAIN = process.env.NEXT_PUBLIC_REACT_APP_AUTH_DOMAIN;
-const DATABASE_URL = process.env.NEXT_PUBLIC_REACT_APP_DATABASE_URL;
-const PROJECT_ID = process.env.NEXT_PUBLIC_REACT_APP_PROJECT_ID;
-const STORAGE_BUCKET = process.env.NEXT_PUBLIC_REACT_APP_STORAGE_BUCKET;
-const MESSAGING_SENDER_ID = process.env.NEXT_PUBLIC_REACT_APP_MESSAGING_SENDER_ID;
-const APP_ID = process.env.NEXT_PUBLIC_REACT_APP_APP_ID;
-const MEASUREMENT_ID = process.env.NEXT_PUBLIC_REACT_APP_MEASUREMENT_ID;
